7662369382966283dc3e893954e190d5641cd04a
rootasjey <jeremiecorpinot@outlook.com>
2019-09-25 15:07:48 +0200

    🚑Fix Checkbox not working well when multiplied

--- src/components/Checkbox.svelte
+++ src/components/Checkbox.svelte
@@ -1,6 +1,18 @@
 <script>
   export let label = '';
   export let checked = false;
+  export let onChange = () => {};
+
+  let cbox;
+  let cboxId = `id-${label}-${Date.now()}`;
+
+  function onClickLabel() {
+    cbox.click()
+  }
+
+  function _onChange(event) {
+    onChange(event);
+  }
 </script>
 
 <style>
@@ -61,16 +73,16 @@
     stroke: #4285f4;
   }
 
-  #cbx:checked + .check svg {
+  input:checked + .check svg {
     stroke: #4285f4;
   }
 
-  #cbx:checked + .check svg path {
+  input:checked + .check svg path {
     stroke-dashoffset: 60;
     transition: all 0.3s linear;
   }
 
-  #cbx:checked + .check svg polyline {
+  input:checked + .check svg polyline {
     stroke-dashoffset: 42;
     transition: all 0.2s linear;
     transition-delay: 0.15s;
@@ -82,12 +94,12 @@
 </style>
 
 <div class="checkbox-container">
-  <input type="checkbox" id="cbx" style="display: none;" bind:checked={checked}>
-  <label for="cbx" class="check">
+  <input type="checkbox" id="{cboxId}" style="display: none;" bind:checked={checked} on:change={_onChange} bind:this={cbox} >
+  <label for="{cboxId}" class="check">
     <svg width="18px" height="18px" viewBox="0 0 18 18">
       <path d="M1,9 L1,3.5 C1,2 2,1 3.5,1 L14.5,1 C16,1 17,2 17,3.5 L17,14.5 C17,16 16,17 14.5,17 L3.5,17 C2,17 1,16 1,14.5 L1,9 Z"></path>
       <polyline points="1 9 7 14 15 4"></polyline>
     </svg>
   </label>
-  <span class="text-label">{label}</span>
+  <span class="text-label" on:click={onClickLabel}>{label}</span>
 </div>
