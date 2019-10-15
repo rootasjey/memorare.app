989445e5fdad59e4a79e632bb89d28a92efcb57d
rootasjey <jeremiecorpinot@outlook.com>
2019-10-15 23:56:42 +0200

    👆Update Snackbar with type and fixes

--- src/components/Snackbar.svelte
+++ src/components/Snackbar.svelte
@@ -34,29 +34,31 @@
 <script>
   import { fly } from 'svelte/transition';
 
+  let action1Fct = () => {};
+  let action2Fct = () => {};
+
   let isActionsActive = false;
   let isAction1Active = false;
   let isAction2Active = false;
 
-  let action1Fct = () => {};
-  let action2Fct = () => {};
 
-  let messageDuration = 4000;
-  let isMessageVisible = false;
-  let messageText = ''
   let action1Text = '';
   let action2Text = '';
-
-  let firstTimer; // setTimeout ref.
-  let secondTimer; // second setTimout ref.
   let currentMsg;
+  let firstTimer; // setTimeout ref.
+  let isMessageVisible = false;
+  let messageDuration = 4000;
+  let messageType = '';
+  let messageText = ''
+  let secondTimer; // second setTimout ref.
 
   const unsubscrbe = observableMessageQueue.subscribe((messagesArray) => {
     if (isMessageVisible) { return; } // wait for the previous message to go away.
     if (messagesQueue.length < 1) { return; }
 
-    currentMsg = messagesQueue[0];
+    currentMsg  = messagesQueue[0];
     messageText = currentMsg.text;
+    messageType = currentMsg.type ? currentMsg.type : '';
 
     if (currentMsg.actions && currentMsg.actions.length > 0) {
       isActionsActive = true;
@@ -85,10 +87,10 @@
 
     isMessageVisible = true;
 
-    firstTimer = window.setTimeout(() => {
+    firstTimer = setTimeout(() => {
       isMessageVisible = false;
 
-      secondTimer = window.setTimeout(() => {
+      secondTimer = setTimeout(() => {
         messagesQueue = messagesQueue.filter((msg) => msg !== currentMsg);
         currentMsg = undefined;
         observableMessageQueue.set(messagesQueue);
@@ -99,10 +101,10 @@
 
   function onDismissMessage() {
     isMessageVisible = false;
-    window.clearTimeout(firstTimer);
-    window.clearTimeout(secondTimer);
+    clearTimeout(firstTimer);
+    clearTimeout(secondTimer);
 
-    window.setTimeout(() => {
+    setTimeout(() => {
       messagesQueue = messagesQueue.filter((msg) => msg !== currentMsg);
       currentMsg = undefined;
       observableMessageQueue.set(messagesQueue);
@@ -111,17 +113,6 @@
 </script>
 
 <style>
-  .snackbar-container {
-    position: absolute;
-    top: 0;
-    left: 0;
-
-    height: 100%;
-    width: 100%;
-
-    pointer-events: none;
-  }
-
   .snackbar {
     color: white;
     background-color: #353b48;
@@ -131,13 +122,27 @@
     padding: 20px;
     margin: 10px;
 
-    position: absolute;
+    position: fixed;
     right: 20px;
     top: 20px;
 
     border-radius: 5px;
     min-width: 200px;
     box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+
+    z-index: 10;
+  }
+
+  .snackbar[data-type="error"] {
+    background-color: #f56498;
+  }
+
+  .snackbar[data-type="info"] {
+    background-color: #00a8ff;
+  }
+
+  .snackbar[data-type="success"] {
+    background-color: #2ecc71;
   }
 
   .snackbar__message {
@@ -154,6 +159,13 @@
     transition: .3s;
   }
 
+  .snackbar[data-type="error"] .snackbar__action,
+  .snackbar[data-type="info"] .snackbar__action,
+  .snackbar[data-type="success"] .snackbar__action {
+    color: #535c68;
+    text-transform: uppercase;
+  }
+
   .snackbar__action:hover {
     color: #f56499bd;
     transition: .3s;
@@ -165,23 +177,24 @@
 
 </style>
 
-<div class="snackbar-container">
-  {#if isMessageVisible}
-    <div class="snackbar" transition:fly={{ y: 10, duration: 500 }} on:click={onDismissMessage} >
-      <div class="snackbar__message">
-        {messageText}
-      </div>
-      {#if isActionsActive}
-        <div class="snackbar__actions">
-          {#if isAction1Active}
-            <div class="snackbar__action snackbar__action1" on:click={action1Fct}>{action1Text}</div>
-          {/if}
-
-          {#if isAction2Active}
-            <div class="snackbar__action snackbar__action2" on:click={action2Fct}>{action2Text}</div>
-          {/if}
-        </div>
-      {/if}
+{#if isMessageVisible}
+  <div class="snackbar" data-type="{messageType}"
+      transition:fly={{ y: 10, duration: 500 }}
+      on:click={onDismissMessage} >
+    <div class="snackbar__message">
+      {messageText}
     </div>
-  {/if}
-</div>
+    {#if isActionsActive}
+      <div class="snackbar__actions">
+        {#if isAction1Active}
+          <div class="snackbar__action snackbar__action1" on:click={action1Fct}>{action1Text}</div>
+        {/if}
+
+        {#if isAction2Active}
+          <div class="snackbar__action snackbar__action2" on:click={action2Fct}>{action2Text}</div>
+        {/if}
+      </div>
+    {/if}
+  </div>
+{/if}
+
