99e022b73670aeaaf94ff7cfe040774149df6970
rootasjey <jeremiecorpinot@outlook.com>
2019-10-25 19:51:50 +0200

    ➕Add full size prop. on Dialog

--- src/components/Dialog.svelte
+++ src/components/Dialog.svelte
@@ -1,7 +1,8 @@
 <script>
   export let active = false;
-  export let maxWidth = -1;
+  export let fullSize = false;
   export let maxHeight = -1;
+  export let maxWidth = -1;
 
   $: maxWidthCSS  = maxWidth > 0 ? `max-width: ${maxWidth}px` : '';
   $: maxHeightCSS = maxHeight > 0 ? `max-height: ${maxHeight}px` : '';
@@ -51,8 +52,8 @@
   }
 
   .dialog-content {
-    min-width: 200px;
-    min-height: 200px;
+    min-width: 150px;
+    min-height: 150px;
     padding: 30px;
 
     background-color: #eee;
@@ -61,6 +62,17 @@
     box-shadow: 0 6px 10px 0 rgba(0,0,0,0.14), 0 1px 18px 0 rgba(0,0,0,0.12), 0 3px 5px -1px rgba(0,0,0,0.20);
 
     z-index: 12;
+  }
+
+  .fullSize .dialog-content {
+    min-width: 100%;
+    min-height: 100%;
+
+    position: absolute;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
   }
 
   .active .dialog-content {
@@ -90,7 +102,7 @@
   }
 </style>
 
-<div class="dialog-component" class:active>
+<div class="dialog-component" class:active class:fullSize>
   <div class="dialog-background" on:click={onClickToClose}></div>
   <div class="dialog-content" style="{styles}">
     <slot name="content"></slot>
