51cfec9c0a94566ebf442d4b39c891c3195ce20f
rootasjey <jeremiecorpinot@outlook.com>
2019-10-27 10:35:51 +0100

    👆Externalize onEnterNextInput(...)

--- src/utils.js
+++ src/utils.js
@@ -4,3 +4,28 @@ export const status = {
   idle      : 'idle',
   loading   : 'loading',
 }
+
+export const onEnterNextInput = (event, domElement) => {
+  const { target } = event;
+  const inputs = domElement.querySelectorAll('input');
+
+  let indexMatch;
+
+  Array
+    .from(inputs)
+    .some((input, index) => {
+      if (input === target) {
+        indexMatch = index;
+        return true;
+      }
+
+      return false;
+    });
+
+  if (indexMatch >= inputs.length) { return; }
+
+  const nextInput = inputs[indexMatch + 1];
+  if (!nextInput || !nextInput.focus) { return; }
+
+  nextInput.focus();
+}
