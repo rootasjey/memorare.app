a333e681483e0e96188acc6b86a5432b1aada7a5
rootasjey <jeremiecorpinot@outlook.com>
2019-10-27 10:42:26 +0100

    ➕Add CapHeader component

---
+++ src/components/CapHeader.svelte
@@ -0,0 +1,30 @@
+<script>
+  export let caption = '';
+  export let title = '';
+
+  export let margin = '';
+
+  let marginCSS = margin ? `margin: ${margin}` : '';
+
+  let styles = `${marginCSS}`;
+</script>
+
+<style>
+  header {
+    margin: 20px 0;
+  }
+
+  .sub-header {
+    font-weight: bold;
+    text-transform: uppercase;
+  }
+
+  h1 {
+    margin-top: 0;
+  }
+</style>
+
+ <header style="{styles}">
+    <span class="sub-header">{caption}</span>
+    <h1>{title}</h1>
+  </header>
