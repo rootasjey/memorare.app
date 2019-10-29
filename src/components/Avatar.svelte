0b13ecc9c6fba2f7282ca160c2d42c8fce927116
rootasjey <jeremiecorpinot@outlook.com>
2019-10-29 10:06:45 +0100

    ➕Add Avatar component

---
+++ src/components/Avatar.svelte
@@ -0,0 +1,104 @@
+<script>
+  export let imgUrl = '';
+  export let name = '';
+  export let onClick = () => {};
+</script>
+
+<style>
+  .avatar-component {
+    height: 150px;
+    width: 150px;
+    margin: 10px;
+    padding: 10px;
+    border-radius: 50%;
+
+    position: relative;
+    overflow: hidden;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    color: white;
+
+    cursor: pointer;
+
+    box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+    transition: .5s;
+  }
+
+  .avatar-component:hover {
+    background-color: #706fd3;
+    box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
+  }
+
+  .avatar-component:hover .avatar-component__name {
+    background-color: #706fd3;
+    opacity: .5;
+    transition: .5s;
+  }
+
+  .avatar-component:hover .avatar-component__img {
+    background-color: #706fd3;
+    opacity: 1;
+    transition: .5s;
+  }
+
+  .avatar-component__name {
+    font-size: 1.2em;
+    font-weight: bold;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    opacity: 1;
+    color: rgba(0,0,0,.5);
+
+    background-color: #eee;
+
+    height: 100%;
+    width: 100%;
+    padding-left: 10px;
+    padding-right: 10px;
+
+    position: absolute;
+    top: 0;
+
+    z-index: 2;
+    transition: .5s;
+  }
+
+  .avatar-component__img {
+    height: 100%;
+    width: 100%;
+
+    opacity: 1;
+    border-radius: 50%;
+
+    position: absolute;
+
+    background-repeat: no-repeat;
+    background-size: cover;
+
+    -moz-filter: url("data:image/svg+xml;utf8,<svg xmlns=\'http://www.w3.org/2000/svg\'><filter id=\'grayscale\'><feColorMatrix type=\'matrix\' values=\'0.3333 0.3333 0.3333 0 0 0.3333 0.3333 0.3333 0 0 0.3333 0.3333 0.3333 0 0 0 0 0 1 0\'/></filter></svg>#grayscale");
+    -o-filter: url("data:image/svg+xml;utf8,<svg xmlns=\'http://www.w3.org/2000/svg\'><filter id=\'grayscale\'><feColorMatrix type=\'matrix\' values=\'0.3333 0.3333 0.3333 0 0 0.3333 0.3333 0.3333 0 0 0.3333 0.3333 0.3333 0 0 0 0 0 1 0\'/></filter></svg>#grayscale");
+    -webkit-filter: grayscale(100%);
+    filter: gray;
+    filter: url("data:image/svg+xml;utf8,<svg xmlns=\'http://www.w3.org/2000/svg\'><filter id=\'grayscale\'><feColorMatrix type=\'matrix\' values=\'0.3333 0.3333 0.3333 0 0 0.3333 0.3333 0.3333 0 0 0.3333 0.3333 0.3333 0 0 0 0 0 1 0\'/></filter></svg>#grayscale");
+
+    transition: .5s;
+  }
+
+    @media (max-width: 380px) {
+    .avatar-component:nth-of-type(1n+4) {
+      display: none;
+    }
+  }
+</style>
+
+<div class="avatar-component" on:click={onClick}>
+  <div class="avatar-component__img" style="background-image: url('{imgUrl}');"></div>
+  <div class="avatar-component__name">{name}
+  </div>
+</div>
--- src/components/Presentation.svelte
+++ src/components/Presentation.svelte
@@ -1,6 +1,8 @@
 <script>
   import { navigate } from 'svelte-routing';
 
+  import Avatar from '../components/Avatar.svelte';
+
   import { client, LIST_AUTHORS } from '../data';
   import { handle } from '../errors';
   import { status } from '../utils';
@@ -137,95 +139,6 @@
       height: 600px;
       top: 500px;
     }
-
-    .author-square:nth-of-type(1n+4) {
-      display: none;
-    }
-  }
-
-  .author-square {
-    height: 150px;
-    width: 150px;
-    margin: 10px;
-    padding: 10px;
-    border-radius: 50%;
-
-    position: relative;
-    overflow: hidden;
-
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    color: white;
-
-    cursor: pointer;
-
-    box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
-    transition: .5s;
-  }
-
-  .author-square:hover {
-    background-color: #706fd3;
-    box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
-  }
-
-  .author-square:hover .author-square__name {
-    background-color: #706fd3;
-    opacity: .5;
-    transition: .5s;
-  }
-
-  .author-square:hover .author-square__img {
-    background-color: #706fd3;
-    opacity: 1;
-    transition: .5s;
-  }
-
-  .author-square__name {
-    font-size: 1.2em;
-    font-weight: bold;
-
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    opacity: 1;
-    color: rgba(0,0,0,.5);
-
-    background-color: #eee;
-
-    height: 100%;
-    width: 100%;
-    padding-left: 10px;
-    padding-right: 10px;
-
-    position: absolute;
-    top: 0;
-
-    z-index: 2;
-    transition: .5s;
-  }
-
-  .author-square__img {
-    height: 100%;
-    width: 100%;
-
-    opacity: 1;
-    border-radius: 50%;
-
-    position: absolute;
-
-    background-repeat: no-repeat;
-    background-size: cover;
-
-    -moz-filter: url("data:image/svg+xml;utf8,<svg xmlns=\'http://www.w3.org/2000/svg\'><filter id=\'grayscale\'><feColorMatrix type=\'matrix\' values=\'0.3333 0.3333 0.3333 0 0 0.3333 0.3333 0.3333 0 0 0.3333 0.3333 0.3333 0 0 0 0 0 1 0\'/></filter></svg>#grayscale");
-    -o-filter: url("data:image/svg+xml;utf8,<svg xmlns=\'http://www.w3.org/2000/svg\'><filter id=\'grayscale\'><feColorMatrix type=\'matrix\' values=\'0.3333 0.3333 0.3333 0 0 0.3333 0.3333 0.3333 0 0 0.3333 0.3333 0.3333 0 0 0 0 0 1 0\'/></filter></svg>#grayscale");
-    -webkit-filter: grayscale(100%);
-    filter: gray;
-    filter: url("data:image/svg+xml;utf8,<svg xmlns=\'http://www.w3.org/2000/svg\'><filter id=\'grayscale\'><feColorMatrix type=\'matrix\' values=\'0.3333 0.3333 0.3333 0 0 0.3333 0.3333 0.3333 0 0 0.3333 0.3333 0.3333 0 0 0 0 0 1 0\'/></filter></svg>#grayscale");
-
-    transition: .5s;
   }
 
   .code-card {
@@ -514,10 +427,10 @@
          <!-- content here -->
       {:else if pageStatus === status.completed}
          {#each authors as author}
-          <div class="author-square">
-            <div class="author-square__img" style="background-image: url('{author.imgUrl}');"></div>
-            <div class="author-square__name"> {author.name} </div>
-          </div>
+          <Avatar
+            name="{author.name}"
+            imgUrl="{author.imgUrl}"
+            />
         {/each}
       {:else}
         <h3>An error occurred while fetching authors.</h3>
