4430f6ef72dae5017bae5b4494b3d181c93f6cbf
rootasjey <jeremiecorpinot@outlook.com>
2019-11-04 21:20:06 +0100

    👆Update Dialog's style (shadow, border)

--- src/components/Dialog.svelte
+++ src/components/Dialog.svelte
@@ -92,8 +92,7 @@
 
     background-color: #eee;
 
-    border-radius: 5px;
-    box-shadow: 0 6px 10px 0 rgba(0,0,0,0.14), 0 1px 18px 0 rgba(0,0,0,0.12), 0 3px 5px -1px rgba(0,0,0,0.20);
+    box-shadow: 6px 6px 0px 0px rgba(0,0,0,0.14), 0px 3px 3px -2px rgba(0,0,0,0.12), 0 1px 8px 0 rgba(0,0,0,0.20);
 
     z-index: 12;
   }
