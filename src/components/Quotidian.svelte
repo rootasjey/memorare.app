0b9473f0e89838db1e883ed9b522733d9262ab1c
rootasjey <jeremiecorpinot@outlook.com>
2019-11-04 19:34:53 +0100

    👆Update Quotidian's style

--- src/components/Quotidian.svelte
+++ src/components/Quotidian.svelte
@@ -3,8 +3,9 @@
 
   import {
     client,
-    QUOTIDIAN
+    QUOTIDIAN,
   } from '../data';
+
   import { handle }   from '../errors'
   import { settings } from '../settings';
   import { status }   from '../utils'
@@ -14,6 +15,8 @@
 
   let pageStatus = status.idle;
   let quotidian = {};
+  let authorImgUrl = '';
+  let bgImg = '';
 
   export let top = '0';
 
@@ -28,6 +31,12 @@
       const response = await client.query({ query: QUOTIDIAN });
       quotidian = response.data.quotidian;
       pageStatus = status.completed;
+
+      authorImgUrl = quotidian.quote.author.imgUrl ?
+        quotidian.quote.author.imgUrl :
+        '/img/user-avatar.png';
+
+    bgImg = `background-image: url('${authorImgUrl}');`;
 
     } catch (error) {
       pageStatus = status.error;
@@ -55,6 +64,10 @@
   async function onShare(quote) {
     console.log('show share ui');
   }
+
+  function onClickAuthor() {
+    navigate(`/author/${quotidian.quote.author.id}`);
+  }
 </script>
 
 <style>
@@ -69,18 +82,27 @@
 
     position: relative;
 
-    box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+    box-shadow: 6px 6px 0px 0px rgba(0,0,0,0.14), 0px 3px 3px -2px rgba(0,0,0,0.12), 0 1px 8px 0 rgba(0,0,0,0.20);
     transition: all 0.3s cubic-bezier(.25,.8,.25,1);
   }
 
   .quotidian:hover {
-    transform: translateY(-5px);
-    box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
+    filter: brightness(95%);
+    transform: translateY(2px);
+    box-shadow: 3px 3px 0px 0px rgba(0,0,0,0.14), 0px 3px 3px -2px rgba(0,0,0,0.12), 0 1px 8px 0 rgba(0,0,0,0.20);
+
+    transition: all 0.3s cubic-bezier(.25,.8,.25,1);
+  }
+
+  .quotidian:active {
+    filter: brightness(90%);
+    transform: translateY(3px);
+    box-shadow: 0px 0px 0px 0px rgba(0,0,0,0.14), 0px 3px 3px -2px rgba(0,0,0,0.12), 0 1px 8px 0 rgba(0,0,0,0.20);
+
     transition: all 0.3s cubic-bezier(.25,.8,.25,1);
   }
 
   footer {
-    text-align: right;
     padding: 20px 0;
   }
 
@@ -101,6 +123,42 @@
   .quotidian__name {
     font-size: 2em;
     font-weight: 600;
+  }
+
+  .author {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+    align-items: center;
+  }
+
+  .author-img {
+    width: 30px;
+    height: 30px;
+
+    background-color: #fff;
+    border: 1px solid #fff;
+    border-radius: 50%;
+    background-size: cover;
+
+    margin-right: 10px;
+
+    box-shadow: 4px 4px 0px 0px rgba(0,0,0,0.14), 0px 3px 3px -2px rgba(0,0,0,0.12), 0 1px 8px 0 rgba(0,0,0,0.20);
+    transition: .3s;
+  }
+
+  .author-img:hover {
+    filter: brightness(98%);
+    transform: translateY(1px);
+    box-shadow: 2px 2px 0px 0px rgba(0,0,0,0.14), 0px 3px 3px -2px rgba(0,0,0,0.12), 0 1px 8px 0 rgba(0,0,0,0.20);
+    transition: .3s;
+  }
+
+  .author-img:active {
+    filter: brightness(96%);
+    transform: translateY(2px);
+    box-shadow: 0px 0px 0px 0px rgba(0,0,0,0.14), 0px 3px 3px -2px rgba(0,0,0,0.12), 0 1px 8px 0 rgba(0,0,0,0.20);
+    transition: .3s;
   }
 
   @media (max-width: 670px) {
@@ -129,7 +187,7 @@
           margin="5px"
           onClick={() => onLike(quotidian)}
           backgroundColor="#f56498"
-          elevation={1} >
+          elevation={2} >
           <svg slot="svg" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="white" viewBox="0 0 24 24">
             <path d="M6.28 3c3.236.001 4.973 3.491 5.72 5.031.75-1.547 2.469-5.021 5.726-5.021 2.058 0 4.274 1.309 4.274 4.182 0 3.442-4.744 7.851-10 13-5.258-5.151-10-9.559-10-13 0-2.676 1.965-4.193 4.28-4.192zm.001-2c-3.183 0-6.281 2.187-6.281 6.192 0 4.661 5.57 9.427 12 15.808 6.43-6.381 12-11.147 12-15.808 0-4.011-3.097-6.182-6.274-6.182-2.204 0-4.446 1.042-5.726 3.238-1.285-2.206-3.522-3.248-5.719-3.248z"/>
           </svg>
@@ -139,7 +197,7 @@
           margin="5px"
           onClick={() => onShare(quotidian)}
           backgroundColor="#f56498"
-          elevation={1} >
+          elevation={2} >
           <svg slot="svg" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="white" viewBox="0 0 24 24">
             <path d="M5 9c1.654 0 3 1.346 3 3s-1.346 3-3 3-3-1.346-3-3 1.346-3 3-3zm0-2c-2.762 0-5 2.239-5 5s2.238 5 5 5 5-2.239 5-5-2.238-5-5-5zm15 9c-1.165 0-2.204.506-2.935 1.301l-5.488-2.927c-.23.636-.549 1.229-.944 1.764l5.488 2.927c-.072.301-.121.611-.121.935 0 2.209 1.791 4 4 4s4-1.791 4-4-1.791-4-4-4zm0 6c-1.103 0-2-.897-2-2s.897-2 2-2 2 .897 2 2-.897 2-2 2zm0-22c-2.209 0-4 1.791-4 4 0 .324.049.634.121.935l-5.488 2.927c.395.536.713 1.128.944 1.764l5.488-2.927c.731.795 1.77 1.301 2.935 1.301 2.209 0 4-1.791 4-4s-1.791-4-4-4zm0 6c-1.103 0-2-.897-2-2s.897-2 2-2 2 .897 2 2-.897 2-2 2z"/>
           </svg>
@@ -149,7 +207,10 @@
       <div class="quotidian__name"> {quotidian.quote.name} </div>
 
       <footer class="quotidian__footer">
-        <h4>{quotidian.quote.author.name}</h4>
+        <div class="author" on:click={onClickAuthor}>
+          <div class="author-img" style="{bgImg}"></div>
+          <span> {quotidian.quote.author.name} </span>
+        </div>
 
         {#if quotidian.quote.references.length}
           <p>{quotidian.quote.references[0].name}</p>
