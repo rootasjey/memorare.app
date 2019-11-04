07c38103bcde2e3d0f83fd9dea974dc5636e5747
rootasjey <jeremiecorpinot@outlook.com>
2019-11-04 20:04:28 +0100

    ➕Add link to edit author, Update style

--- src/routes/Author.svelte
+++ src/routes/Author.svelte
@@ -1,5 +1,8 @@
 <script>
+  import { navigate } from 'svelte-routing';
+
   import Avatar from '../components/Avatar.svelte';
+
   import {
     hide as hideHeader,
     show as showHeader
@@ -44,6 +47,10 @@
   function goBack() {
     showHeader();
     window.history.back();
+  }
+
+  function onEditAuthor() {
+    navigate(`/edit/author/${id}`);
   }
 </script>
 
@@ -106,24 +113,56 @@
     top: 200px;
   }
 
-  .close-icon {
-    position: absolute;
-    top: 30px;
-    right: 30px;
-
+  .square-icon {
     cursor: pointer;
     transform-origin: center;
+
+    padding: 20px;
+    background-color: #f56498;
 
     display: flex;
     justify-content: center;
     align-items: center;
+
+    box-shadow: 6px 6px 0px 0px rgba(0,0,0,0.14), 0px 3px 3px -2px rgba(0,0,0,0.12), 0 1px 8px 0 rgba(0,0,0,0.20);
     transition: .3s;
   }
 
-  .close-icon:hover {
+  .square-icon:hover {
+    top: 32px;
+    filter: brightness(95%);
+    box-shadow: 3px 3px 0px 0px rgba(0,0,0,0.14), 0px 3px 3px -2px rgba(0,0,0,0.12), 0 1px 8px 0 rgba(0,0,0,0.20);
+    transition: .3s;
+  }
+
+  .square-icon:active {
+    top: 34px;
+    filter: brightness(90%);
+    box-shadow: 0px 0px 0px 0px rgba(0,0,0,0.14), 0px 3px 3px -2px rgba(0,0,0,0.12), 0 1px 8px 0 rgba(0,0,0,0.20);
+    transition: .3s;
+  }
+
+  .square-icon > svg {
+    transform-origin: center;
+    transition: .3s;
+  }
+
+  .square-icon > svg:hover {
     transform: rotate(180deg);
     transform-origin: center;
     transition: .3s;
+  }
+
+  .close-icon {
+    position: absolute;
+    top: 30px;
+    right: 30px;
+  }
+
+  .edit-icon {
+    position: absolute;
+    top: 30px;
+    right: 105px;
   }
 
   .quote-card-compact {
@@ -148,9 +187,13 @@
   <header>
     <div class="header__bg" style="background-image: url('{author.imgUrl}')"></div>
 
-    <div class="close-icon" on:click={goBack}>
-      <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#fff" viewBox="0 0 24 24"><path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"/>
+    <div class="square-icon close-icon" on:click={goBack}>
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#fff" viewBox="0 0 24 24"><path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"/>
       </svg>
+    </div>
+
+    <div class="square-icon edit-icon" on:click={onEditAuthor}>
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#fff" viewBox="0 0 24 24"><path d="M1.439 16.873l-1.439 7.127 7.128-1.437 16.873-16.872-5.69-5.69-16.872 16.872zm4.702 3.848l-3.582.724.721-3.584 2.861 2.86zm15.031-15.032l-13.617 13.618-2.86-2.861 10.825-10.826 2.846 2.846 1.414-1.414-2.846-2.846 1.377-1.377 2.861 2.86z"/></svg>
     </div>
 
     <div class="header__content">
