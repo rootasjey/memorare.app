2e36145986d46c59728da178becaec1e4b0daf84
rootasjey <jeremiecorpinot@outlook.com>
2019-11-17 23:29:22 +0100

    ➕Add order toggle button on TempQuotes

--- src/routes/TempQuotes.svelte
+++ src/routes/TempQuotes.svelte
@@ -23,6 +23,7 @@
 
   let hasMoreData     = true;
   let limit           = 10;
+  let order           = 1;
   let pageStatus      = status.loading;
   let selectedQuoteId = -1;
   let skip            = 0;
@@ -50,7 +51,7 @@
     try {
       const response = await client.query({
         query: TEMP_QUOTES_ADMIN,
-        variables: { limit, skip },
+        variables: { limit, order, skip },
       });
 
       tempQuotes = response.data.tempQuotesAdmin.entries;
@@ -99,7 +100,7 @@
     try {
       const response = await client.query({
         query: TEMP_QUOTES_ADMIN,
-        variables: { limit, skip },
+        variables: { limit, order, skip },
         fetchPolicy: 'network-only',
       });
 
@@ -129,7 +130,7 @@
     try {
       const response = await client.query({
         query: TEMP_QUOTES_ADMIN,
-        variables: { limit, skip },
+        variables: { limit, order, skip },
         fetchPolicy: 'network-only',
       });
 
@@ -174,6 +175,11 @@
     }
   }
 
+  function onToggleOrder() {
+    order = order === 1 ? -1 : 1;
+    onRefresh();
+  }
+
   async function onValidate(quote) {
     const { id, validation: { status: currentStatus } } = quote;
 
@@ -213,12 +219,6 @@
 </script>
 
 <style>
-  .temp-quotes-page__content {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
-
   .content__buttons-container {
     margin: 25px 0;
   }
@@ -236,12 +236,6 @@
     font-weight: 100;
 
     margin-top: -10px;
-  }
-
-  .temp-quotes-page {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
   }
 
   .list-temp-quote {
@@ -296,6 +290,24 @@
     display:flex;
   }
 
+  .row-buttons {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-bottom: 40px;
+  }
+
+  .temp-quotes-page {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .temp-quotes-page__content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
 </style>
 
 <div class="temp-quotes-page">
@@ -312,6 +324,36 @@
         </div>
       {:else if pageStatus === status.completed}
         <div class="content__buttons-container">
+          <div class="row-buttons">
+            {#if order === 1}
+              <IconButton
+                on:click={onToggleOrder}>
+                <svg
+                  slot="svg"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  fill="#fff"
+                  viewBox="0 0 24 24">
+                  <path d="M6 21l6-8h-4v-10h-4v10h-4l6 8zm16-12h-8v-2h8v2zm2-6h-10v2h10v-2zm-4 8h-6v2h6v-2zm-2 4h-4v2h4v-2zm-2 4h-2v2h2v-2z"/>
+                </svg>
+              </IconButton>
+            {:else}
+              <IconButton
+                on:click={onToggleOrder}>
+                <svg
+                  slot="svg"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  fill="#fff"
+                  viewBox="0 0 24 24">
+                  <path d="M6 3l-6 8h4v10h4v-10h4l-6-8zm16 6h-8v-2h8v2zm2-6h-10v2h10v-2zm-4 8h-6v2h6v-2zm-2 4h-4v2h4v-2zm-2 4h-2v2h2v-2z"/>
+                </svg>
+              </IconButton>
+            {/if}
+          </div>
+
           <RectButton outlined={true} value="refresh" on:click={onRefresh} />
         </div>
 
